1fa55f2ed1bd2b4760cda418ba24bf397c58f289
Alok Behera <alokbehera271@gmail.com>
2026-04-29 23:46:45 +0530

    removed license accept line in script/login.bat file

--- scripts/login.bat
+++ scripts/login.bat
@@ -1,3 +1,2 @@
-C:\apiccli\apic licenses --accept
 C:\apiccli\apic client-creds:set C:\apiccli\credentials.json
 C:\apiccli\apic login --sso --context provider --server https://platform-api.us-east-a.apiconnect.automation.ibm.com --apiKey 2c2e901c-c56c-4805-b554-9caddb5dc091
