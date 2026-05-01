eaa5b7e95bc2adccf134521e49e3d7c0a9b3905a
Alok Behera <alokbehera271@gmail.com>
2026-05-01 12:39:28 +0530

    modified scripts\publish batch file

--- scripts/publish.bat
+++ scripts/publish.bat
@@ -1,1 +1,1 @@
-C:\apiccli\apic products:publish --catalog DEVELOPMENT --org api-connect-os-1 --server https://platform-api.us-east-a.apiconnect.automation.ibm.com  api_ex1_product_1.0.0.yaml --debug --scope space --space team1
+C:\apiccli\apic products:publish --catalog DEV --org api-connect-os-1 --server https://platform-api.ap-south-a.apiconnect.automation.ibm.com  api_ex1_product_1.0.0.yaml --debug
